4dfcadeff5adf7e6ee483cbf2bdfcae5dab34ad1
thuviengithub0208 <khoaphp@yahoo.com>
2018-05-30 20:08:46 +0700

    "Thu vien conver gson"

--- app/src/main/java/com/ptp/phamtanphat/retrofit21903/Service/RetrofitInit.java
+++ app/src/main/java/com/ptp/phamtanphat/retrofit21903/Service/RetrofitInit.java
@@ -1,11 +1,16 @@
 package com.ptp.phamtanphat.retrofit21903.Service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by KhoaPhamPC on 30/5/2018.
@@ -22,7 +27,11 @@ public class RetrofitInit {
                                     .retryOnConnectionFailure(true)
                                     .protocols(Arrays.asList(Protocol.HTTP_1_1))
                                     .build();
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
         retrofit = new Retrofit.Builder()
-                    .
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory)
     }
 }
