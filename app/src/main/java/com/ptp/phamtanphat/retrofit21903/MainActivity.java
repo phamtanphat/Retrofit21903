d821681018825bdc57d4b99e9cea953416af1e91
thuviengithub0208 <khoaphp@yahoo.com>
2018-05-30 21:02:15 +0700

    "Thu vien conver gson"

--- app/src/main/java/com/ptp/phamtanphat/retrofit21903/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/retrofit21903/MainActivity.java
@@ -2,6 +2,15 @@ package com.ptp.phamtanphat.retrofit21903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ptp.phamtanphat.retrofit21903.Service.DataAPI;
+import com.ptp.phamtanphat.retrofit21903.Service.RetrofitAPI;
+import com.ptp.phamtanphat.retrofit21903.Service.RetrofitInit;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+//        DataAPI dataAPI = RetrofitAPI.getData();
+//        Call<Demo1> callback = dataAPI.getDataDemo1();
+//        callback.enqueue(new Callback<Demo1>() {
+//            @Override
+//            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+//                Demo1 demo1 = response.body();
+//                Log.d("BBB",demo1.getFanpage());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Demo1> call, Throwable t) {
+//                Log.d("BBB",t.getMessage());
+//            }
+//        });
+        DataAPI dataAPI = RetrofitAPI.getData();
+        Call<Demo2> callback = dataAPI.getDataDemo2();
+        callback.enqueue(new Callback<Demo2>() {
+            @Override
+            public void onResponse(Call<Demo2> call, Response<Demo2> response) {
+                Demo2 demo2 = response.body();
+                Log.d("BBB",demo2.getDanhsach().get(0).getKhoahoc());
+            }
 
+            @Override
+            public void onFailure(Call<Demo2> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/ptp/phamtanphat/retrofit21903/Service/DataAPI.java
+++ app/src/main/java/com/ptp/phamtanphat/retrofit21903/Service/DataAPI.java
@@ -1,6 +1,7 @@
 package com.ptp.phamtanphat.retrofit21903.Service;
 
 import com.ptp.phamtanphat.retrofit21903.Demo1;
+import com.ptp.phamtanphat.retrofit21903.Demo2;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -13,4 +14,7 @@ public interface DataAPI {
 
     @GET("demo1.json")
     Call<Demo1> getDataDemo1();
+
+    @GET("demo2.json")
+    Call<Demo2> getDataDemo2();
 }
